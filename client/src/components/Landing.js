46d8bb46bff9bbceb5e5822cc6979cbbe6e2ba2a
VitaliyBulyma <55520185+VitaliyBulyma@users.noreply.github.com>
2020-08-28 11:00:58 -0400

    landing page text updated

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 const Landing = ()=> {
     return(
-        <div className="m-4"> <p>This is landing page</p></div>
+        <div className="m-4">
+        <div className="card">
+          <div className="card-body text-center">
+            <h4 className="card-title">Landing Page</h4>
+            <p>Content of the landing page accessible by all users</p>
+          </div>
+        </div>
+      </div>
     );
 };
 
